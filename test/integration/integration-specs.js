b83575fe178c5f271c7afa474a22b0bf14965811
Matt Greer <matt.e.greer@gmail.com>
2014-12-02 11:56:20 -0700

    Now bookshelf based

--- test/integration/integration-specs.js
+++ test/integration/integration-specs.js
@@ -1,24 +1,39 @@
 var _ = require('lodash');
 var bluebird = require('bluebird');
 var sqlFixtures = require('sql-fixtures');
+var bookshelf = require('bookshelf');
 var Bookends = require('../..');
+
+var fixtureGenerator = null;
+var knex = null;
+var bookends = null;
+
+var Parent, Child;
 
 module.exports = function(dbConfig) {
   describe('bookends', function() {
     this.enableTimeouts(false);
 
     before(function() {
-      this.fixtureGenerator = new sqlFixtures(dbConfig);
-      this.knex = this.fixtureGenerator.knex;
-      this.bookends = new Bookends(this.knex);
+      fixtureGenerator = new sqlFixtures(dbConfig);
+      knex = fixtureGenerator.knex;
+      bookends = new Bookends(knex);
 
+      var db = bookshelf(knex);
 
+      Child = db.Model.extend({
+        tableName: 'child'
+      });
+
+      Parent = db.Model.extend({
+        tableName: 'parent',
+        children: function() {
+          return this.hasMany(Child, 'parent_id');
+        }
+      });
     });
 
     beforeEach(function(done) {
-      var knex = this.knex;
-      var me = this;
-
       var dropPromises = [
         knex.schema.dropTableIfExists('child'),
         knex.schema.dropTableIfExists('parent')
@@ -42,7 +57,7 @@ module.exports = function(dbConfig) {
     });
 
     after(function(done) {
-      this.fixtureGenerator.destroy(done);
+      fixtureGenerator.destroy(done);
     });
 
     describe('hydration', function() {
@@ -54,18 +69,17 @@ module.exports = function(dbConfig) {
           }
         };
 
-        var bookends = this.bookends;
-        this.fixtureGenerator.create(dataSpec).then(function(result) {
-          bookends.hydrate('parent', ['string_column']).then(function(records) {
-            var record = records.pop();
-            expect(record.string_column).to.equal('value1');
-            expect(record).to.not.have.property('second_string_column');
+        fixtureGenerator.create(dataSpec).then(function(result) {
+          bookends.hydrate(Parent, ['string_column']).then(function(records) {
+            expect(records.length).to.equal(1);
+            expect(records[0].string_column).to.equal('value1');
+            expect(records[0]).to.not.have.property('second_string_column');
             done();
           });
         });
       });
 
-      it('should hydrate a child relation', function(done) {
+      it('should hydrate a child relation with no unexpected columns returned', function(done) {
         var dataSpec = {
           parent: {
               string_column: 'value1'
@@ -76,11 +90,47 @@ module.exports = function(dbConfig) {
           }
         };
 
-        var bookends = this.bookends;
-        this.fixtureGenerator.create(dataSpec).then(function(result) {
-          bookends.hydrate('parent', ['child.string_column']).then(function(records) {
+        fixtureGenerator.create(dataSpec).then(function(result) {
+          // 'children=[string_column]'
+          var hydration = [
+            { relation: 'children', hydration: ['string_column']}
+          ];
+
+          bookends.hydrate(Parent, hydration).then(function(records) {
             var record = records.pop();
-            expect(record.child[0].string_column).to.equal('value2');
+            expect(record.id).to.be.a('number');
+            expect(record.children[0].string_column).to.equal('value2');
+            expect(record.children[0]).to.not.have.property('id');
+            expect(record.children[0]).to.not.have.property('parent_id');
+            done();
+          });
+        });
+      });
+
+      it('should hydrate a child relation and return the foreign key column if it was requested', function(done) {
+        var dataSpec = {
+          parent: {
+            string_column: 'value1'
+          },
+          child: {
+            parent_id: 'parent:0',
+            string_column: 'value2'
+          }
+        };
+
+        fixtureGenerator.create(dataSpec).then(function(result) {
+          // 'children=[string_column]'
+          var hydration = [
+            { relation: 'children', hydration: ['string_column', 'parent_id']}
+          ];
+
+          bookends.hydrate(Parent, hydration).then(function(records) {
+            var record = records.pop();
+            expect(record.id).to.be.a('number');
+            expect(record.children[0].string_column).to.equal('value2');
+            expect(record.children[0]).to.not.have.property('id');
+            expect(record.children[0].parent_id).to.equal(record.id);
+            done();
           });
         });
       });

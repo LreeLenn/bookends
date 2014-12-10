ad145b449b18c19052d9a67fcbededda1fafffdd
Matt Greer <matt.e.greer@gmail.com>
2014-12-10 10:16:14 -0700

    feat(bookends), version(0.10.0): multiple where clauses

--- test/integration/integration-specs.js
+++ test/integration/integration-specs.js
@@ -395,6 +395,33 @@ module.exports = function(dbConfig) {
           });
         });
       });
+
+      it('should allow multiple wheres', function(done) {
+        var dataSpec = {
+          root: [{
+            string_column: 'root0',
+            second_string_column: 'second'
+          }, {
+            string_column: 'root1',
+            second_string_column: 'second'
+          }]
+        };
+
+        fixtureGenerator.create(dataSpec).then(function(result) {
+          var options = {
+            where: [
+             { string_column: 'root0' },
+             { second_string_column: 'second' }
+            ]
+          };
+
+          bookends.hydrate(Root, options, ['string_column']).then(function(result) {
+            expect(result.records.length).to.equal(1);
+            expect(result.records[0].string_column).to.equal('root0');
+            done();
+          });
+        });
+      });
     });
 
     describe('orderBy', function() {

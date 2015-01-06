806858385aff812c34ff78ab8b3142829fb3a104
Matt Greer <matt.e.greer@gmail.com>
2015-01-06 13:40:28 -0700

    feat(site) record view now has commas

--- site/src/cljs/demo/components/records.cljs
+++ site/src/cljs/demo/components/records.cljs
@@ -11,10 +11,12 @@
   (or (string? obj)
       (number? obj)))
 
-(defn primitive-view [p]
-  (cond
-    (string? p) [:span.primitive-string (str "\"" p "\"")]
-    (number? p) [:span.primitive-number p]))
+(defn primitive-view [p needs-comma]
+  [:span
+   (cond
+     (string? p) [:span.primitive-string (str "\"" p "\"")]
+     (number? p) [:span.primitive-number p])
+   (when needs-comma ",")])
 
 (defn expander [collapsed-atom]
   [:span.expander {:on-click #(swap! collapsed-atom not)} 
@@ -29,7 +31,7 @@
 
 (defn array-view []
   (let [collapsed (atom false)]
-    (fn [title array] 
+    (fn [title array needs-comma] 
       [:div.array-view.expander-container
        [expander collapsed]
        [title-view title "["]
@@ -37,36 +39,44 @@
          "…"
          [:div.indent
           (for [object array]
-            (if (primitive? object)
+            (let [is-last (= (last array) object)
+                  needs-comma (not is-last)]
+             (if (primitive? object)
               [:div
-               [primitive-view object]]
-              [object-view nil object]))])
-       "]"])))
+               [primitive-view object needs-comma]]
+              [object-view nil object needs-comma])))])
+       "]"
+       (when needs-comma ",")])))
 
-(defn key-value-view [keyname value]
+(defn key-value-view [keyname value needs-comma]
   [:div
    [:span.object-view-entry-key (str keyname ":")]
-   [:span.object-view-entry-value [primitive-view value]]])
+   [:span.object-view-entry-value [primitive-view value]]
+   (when needs-comma ",")])
 
 (defn object-view []
   (let [collapsed (atom false)]
-    (fn [title object] 
+    (fn [title object needs-comma] 
       [:div.object-view.expander-container
        [expander collapsed]
        [title-view title "{"]
        (if @collapsed
          "…"
          [:div.indent 
-          (for [key (sort-keys object)] 
-            [:div.object-view-entry
-             (let [keyname (name key)
-                   value (object key)] 
-               (cond
-                 (vector? value) [array-view keyname value]
-                 (map? value)    [object-view keyname value]
-                 :else           [key-value-view keyname value]))])])
-       "}"])))
+          (let [keys (sort-keys object)] 
+            (for [key keys] 
+              [:div.object-view-entry
+               (let [keyname (name key)
+                     value (object key)
+                     is-last (= (last keys) key)
+                     needs-comma (not is-last)] 
+                 (cond
+                   (vector? value) [array-view keyname value needs-comma]
+                   (map? value)    [object-view keyname value needs-comma]
+                   :else           [key-value-view keyname value needs-comma]))]))])
+       "}"
+       (when needs-comma ",")])))
 
 (defn cmp [object]
   [:div.records-list
-   [object-view nil object]])
+   [object-view nil object false]])
